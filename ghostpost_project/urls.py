565e2f20daa342c923f4fa39619f5c96ba1b2768
luisfff29 <luisfff29@gmail.com>
2020-05-13 11:26:44 -0500

    display index page

--- ghostpost_project/urls.py
+++ ghostpost_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ghostpost_app.views import urlpatterns as more_urls
+from ghostpost_app.urls import urlpatterns as more_urls
 
 
 urlpatterns = [
