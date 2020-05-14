04df8be0374f0d402c88fd5f512ff669ec7d2d8a
luisfff29 <luisfff29@gmail.com>
2020-05-13 21:14:24 -0500

    improve submit form

--- ghostpost_app/forms.py
+++ ghostpost_app/forms.py
@@ -5,11 +5,11 @@ from ghostpost_app.models import GhostPost
 class AddPost(forms.ModelForm):
     class Meta:
         model = GhostPost
-        fields = '__all__'
+        fields = ['text', 'boast_or_roast']
 
 # class GhostPost(models.Model):
 #     text = models.CharField(max_length=280)
-#     choice = models.BooleanField(help_text='Check: Boast, Uncheck: Roast')
+#     boast_or_roast = models.BooleanField(help_text='Check: Boast, Uncheck: Roast')
 #     up_vote = models.IntegerField(default=0)
 #     down_vote = models.IntegerField(default=0)
 #     date = models.DateTimeField(default=timezone.now)
