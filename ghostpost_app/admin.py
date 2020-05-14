e23d80fe32e01377925b5e618e75efd36aa1f4aa
luisfff29 <luisfff29@gmail.com>
2020-05-13 20:36:27 -0500

    create model

--- ghostpost_app/admin.py
+++ ghostpost_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from ghostpost_app.models import GhostPost
+
 
 # Register your models here.
+admin.site.register(GhostPost)
--- ghostpost_app/models.py
+++ ghostpost_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class GhostPost(models.Model):
+    text = models.CharField(max_length=280)
+    choice = models.BooleanField(help_text='Check: Boast, Uncheck: Roast')
+    up_vote = models.IntegerField(default=0)
+    down_vote = models.IntegerField(default=0)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.choice
