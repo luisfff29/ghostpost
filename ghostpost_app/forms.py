5eaf417a5c7df83c508e01a86ca360def2b69536
luisfff29 <luisfff29@gmail.com>
2020-05-15 09:54:07 -0500

    add filter/order_by

--- ghostpost_app/forms.py
+++ ghostpost_app/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from ghostpost_app.models import GhostModel
 
 
+CHOICES = ((('---------', '---------')), ('all_boasts', 'All Boasts'), ('all_roasts', 'All Roasts'),
+           ('up_vote', 'Up votes'), ('down_vote', 'Down votes'), ('vote_difference', 'Vote difference'))
+
+
 class GhostForm(forms.ModelForm):
     text = forms.CharField(widget=forms.Textarea(
         attrs={
@@ -12,3 +16,11 @@ class GhostForm(forms.ModelForm):
     class Meta:
         model = GhostModel
         fields = ['boast_or_roast', 'text']
+
+
+class AddFilter(forms.Form):
+    add_filter = forms.ChoiceField(choices=CHOICES, widget=forms.Select(
+        attrs={
+            'onchange': 'submit()'
+        }
+    ))
--- ghostpost_app/views.py
+++ ghostpost_app/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
 from ghostpost_app.models import GhostModel
-from ghostpost_app.forms import GhostForm
+from ghostpost_app.forms import GhostForm, AddFilter
 
 
 # Create your views here.
 def index(request):
-    data = GhostModel.objects.all()
-    return render(request, 'index.html', {'data': data})
+    if request.method == 'POST':
+        form = AddFilter(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            if data['add_filter'] == '---------':
+                result = GhostModel.objects.order_by('date')
+            elif data['add_filter'] == 'all_boasts':
+                result = GhostModel.objects.order_by(
+                    'date').filter(boast_or_roast=True)
+            elif data['add_filter'] == 'all_roasts':
+                result = GhostModel.objects.order_by(
+                    'date').filter(boast_or_roast=False)
+            elif data['add_filter'] == 'up_vote':
+                result = GhostModel.objects.order_by('-up_vote', 'date')
+            elif data['add_filter'] == 'down_vote':
+                result = GhostModel.objects.order_by('-down_vote', 'date')
+            elif data['add_filter'] == 'vote_difference':
+                result = GhostModel.objects.extra(
+                    select={'diff': 'up_vote - down_vote'}).order_by('-diff')
+        return render(request, 'index.html', {'result': result, 'form': form})
+
+    form = AddFilter()
+    result = GhostModel.objects.order_by('date')
+
+    return render(request, 'index.html', {'result': result, 'form': form})
 
 
 def addpost(request):
